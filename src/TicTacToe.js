3995bb37cb36115bcde222083a11c5445c286dd7
earosselot <earosselot@gmail.com>
2021-08-20 11:08:33 -0300

    AI unbeatable with minimax algorithm implementation.

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -48,10 +48,6 @@ function TicTacToe() {
 export default TicTacToe;
 
 
-function getMachineMove(gameBoard) {
-  return gameBoard.findIndex(element => element === null)
-}
-
 const winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 function checkEndGame(gameBoard) {
@@ -77,3 +73,60 @@ function getGameWinner(gameBoard) {
   return ;
 }
 
+// Computer IA
+
+const score = {
+  'Computer': 1,
+  'Player': -1,
+  'tie': 0
+}
+
+function getMachineMove(gameBoard) {
+  let bestScore = -Infinity
+  let bestMove = null
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (!gameBoard[i]) {
+      gameBoard[i] = 'O'
+      let moveScore = minimax(gameBoard, false)
+      gameBoard[i] = null
+      if (moveScore > bestScore) {
+        bestScore = moveScore
+        bestMove = i
+      }
+    }
+  }
+  return bestMove
+}
+
+function minimax(gameBoard, isMaximizer) {
+
+  const gameEnded = checkEndGame(gameBoard)
+  if (gameEnded) {
+    const winner = getGameWinner(gameBoard)
+    return winner ? score[winner] : score['tie']
+  }
+
+  if (isMaximizer) {
+    let bestScore = -Infinity
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (!gameBoard[i]) {
+        gameBoard[i] = 'O'
+        let moveScore = minimax(gameBoard, false)
+        gameBoard[i] = null
+        bestScore = Math.max(moveScore, bestScore)
+      }
+    }
+    return bestScore
+  } else {
+    let bestScore = Infinity
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (!gameBoard[i]) {
+        gameBoard[i] = 'X'
+        let moveScore = minimax(gameBoard, true)
+        gameBoard[i] = null
+        bestScore = Math.min(moveScore, bestScore)
+      }
+    }
+    return bestScore
+  }
+}
